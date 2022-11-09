a31e4e91f949f1ee098b9e6ff6cd30fbb164b757
sycho09 <wyswid@naver.com>
2022-11-10 02:31:16 +0900

    feat: add ref to highlight text line with keyboard event

--- src/utils/hooks/useHandleKeyup.ts
+++ src/utils/hooks/useHandleKeyup.ts
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ISearchedList } from '../Type';
 
-const useHandleKeyup = (list: ISearchedList[]) => {
+const useHandleKeyup = (
+  list: ISearchedList[],
+  reset: React.Dispatch<React.SetStateAction<ISearchedList[]>>
+) => {
   const [keyIndex, setKeyIndex] = useState(-1);
+  const listRef = useRef<HTMLUListElement>(null);
 
   const handleKeyup = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'ArrowDown') {
@@ -11,13 +15,17 @@ const useHandleKeyup = (list: ISearchedList[]) => {
     if (e.key === 'ArrowUp' && keyIndex > -1) {
       setKeyIndex((prev) => prev - 1);
     }
+    if (e.key === 'Escape' && keyIndex > -1) {
+      setKeyIndex(-1);
+      reset([]);
+    }
   };
 
   useEffect(() => {
     setKeyIndex(-1);
   }, [list]);
 
-  return { handleKeyup, keyIndex };
+  return { handleKeyup, keyIndex, listRef };
 };
 
 export default useHandleKeyup;
