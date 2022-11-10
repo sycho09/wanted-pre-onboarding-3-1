431b7268b8a38963edba05c62e2a35f4650462f2
sycho09 <wyswid@naver.com>
2022-11-10 10:54:51 +0900

    feat: block arrow up and down over the length of the list

--- src/utils/hooks/useHandleKeyup.ts
+++ src/utils/hooks/useHandleKeyup.ts
@@ -1,19 +1,23 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { Dispatch } from 'redux';
+import { Action } from '../../state/actions';
 import { ISearchedList } from '../Type';
 
 const useHandleKeyup = (
   list: ISearchedList[],
-  reset: React.Dispatch<React.SetStateAction<ISearchedList[]>>
+  reset: (searchList: ISearchedList[]) => (dispatch: Dispatch<Action>) => void
 ) => {
   const [keyIndex, setKeyIndex] = useState(-1);
   const listRef = useRef<HTMLUListElement>(null);
 
   const handleKeyup = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'ArrowDown') {
-      setKeyIndex((prev) => prev + 1);
+      keyIndex > list.length - 2
+        ? setKeyIndex(() => list.length - 1)
+        : setKeyIndex((prev) => prev + 1);
     }
     if (e.key === 'ArrowUp' && keyIndex > -1) {
-      setKeyIndex((prev) => prev - 1);
+      keyIndex < 1 ? setKeyIndex(() => 0) : setKeyIndex((prev) => prev - 1);
     }
     if (e.key === 'Escape') {
       setKeyIndex(-1);
